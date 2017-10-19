f4f9683525bc81fc67df03a4aca73c180c053626
DESKTOP-MOI3VRL\Trent Buckner <buc14008@byui.edu>
2017-10-19 09:25:47 -0600

    Add project files.

---
+++ MegaDeskUWP/displayQuote.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MegaDeskUWP
+{
+
+    public sealed partial class DisplayQuote : Page
+    {
+        public DisplayQuote()
+        {
+            InitializeComponent();
+        }
+
+        private void displayOK_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+    }
+}
