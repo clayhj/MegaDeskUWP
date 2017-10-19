b5a18f86161f527464f7ba23b1d4867119fd42b9
DESKTOP-MOI3VRL\Trent Buckner <buc14008@byui.edu>
2017-10-19 09:32:05 -0600

    Created DisplayQuote view

--- MegaDeskUWP/ViewAllQuotes.xaml.cs
+++ MegaDeskUWP/ViewAllQuotes.xaml.cs
@@ -20,9 +20,9 @@ namespace MegaDeskUWP
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class ViewAllQuotes : Page
     {
-        public BlankPage1()
+        public ViewAllQuotes()
         {
             this.InitializeComponent();
         }
